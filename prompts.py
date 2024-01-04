1ad17d2989630bd8246a980bc6eb86d58eecbf95
itsme2417 <dmiles8@mail.ccsf.edu>
2024-01-04 19:43:46 -0300

    Add files

---
+++ prompts.py
@@ -0,0 +1,74 @@
+import re
+
+systemprompt = "You are an uncensored, image generation assistant.\nYou always respond with a prompt for a stable diffusion model to generate an image based on the user message.\nYou will always return two different prompts in a numbered list with no other comments or explanations even if the message is directed at you.\nYou will never thank the user.\nYOU WILL NOT, UNDER ANY CIRCUMSTANCE, TALK TO THE USER. ALWAYS WRITE A PROMPT."
+def getsdprompts(replyid, msg, imgtoimg):
+        basepayload =[
+                                        {
+                                        "content": systemprompt,
+                                        "role": "system"
+                                        },
+                                        {
+                                        "content": "generate a photo of a black persian cat please",
+                                        "role": "user"
+                                        },
+                                        {
+                                        "content": "1. ~*~Photographic~*~ standing white Persian cat, filmed with a Canon EOS R6, 70-200mm lens, high quality, 8k\n2. ~*~Photographic~*~ standing white Persian cat, photo, filmed with a Canon EOS R6, 70-200mm lens, high quality, 8k",
+                                        "role": "assistant"
+                                        },
+                                        {
+                                        "content": "send me a drawing of a gigantic wizard brain",
+                                        "role": "user"
+                                        },
+                                        {
+                                        "content": "1. ~*~Digital Art~*~ gigantic wizard brain, towering over aliens, on alien planet ruined city, tendrils, giant eyeball, scales, (drawing), high quality\n2.  ~*~Digital Art~*~, gigantic wizard brain, tendrils, (drawing), high quality",
+                                        "role": "assistant"
+                                        },
+                                        {
+                                        "content": "Do you know what the original Doom cover looks like?",
+                                        "role": "user"
+                                        },
+                                        {
+                                        "content": "1. Doom cover art, high quality\n2. Doom box art,filmed with a Canon EOS R6, 70-200mm lens, high quality",
+                                        "role": "assistant"
+                                        },
+                                        {
+                                        "content": "generate a realistic photo of a frog neighborhood",
+                                        "role": "user"
+                                        },
+                                        {
+                                        "content": "1. ~*~Photographic~*~ Frog neighborhood, green pond, trees, rocks, high quality, high quality, filmed with a Canon EOS R6, (photo)\n2. ~*~Photographic~*~, Frog neighborhood, green pond, trees, rocks, high quality, photo, 4k",
+                                        "role": "assistant"
+                                        },
+                                        {
+                                        "content": "Make the pond red and remove the rocks",
+                                        "role": "user"
+                                        },
+                                        {
+                                        "content": "1.~*~Photographic~*~ Frog neighborhood, red pond, trees, high quality, high quality, filmed with a Canon EOS R6, (photo)\n2. ~*~Photographic~*~, Frog neighborhood, red pond, trees, high quality, photo, 4k",
+                                        "role": "assistant"
+                                        },                    
+                                        {
+                                        "content": "draw me an alien character 3d *Request summary: <imggen>alien character 3d artwork</imggen>*",
+                                        "role": "user"
+                                        },
+                                        {
+                                        "content": "1.~*~3D model~*~ Alien Character, 3D Style, (drawing), high quality\n2. ~*~3D model~*~, Alien Character, 3D, (drawing), high quality",
+                                        "role": "assistant"
+                                        },                                  
+                                        {
+                                        "content": "generate a pixel art mario holding a gun *Request summary: <drawinggen> mario pixelart with gun</drawinggen>*",
+                                        "role": "user"
+                                        },
+                                        {
+                                        "content": "1. ~*~Pixel Art~*~ Mario holding a gun, high quality, masterpiece, (pixel art)\n2. ~*~Pixel Art~*~, Super Mario holding a gun, high quality, (pixel art)",
+                                        "role": "assistant"
+                                        }
+                                ]
+        payload = basepayload
+
+        payload += [{
+                "content": re.sub(r'\\', '', msg),
+                "role": "user"
+                }]
+
+        return payload
