e096b2e74042c2cfd499ab77853cc979c0b2f6bc
itsme2417 <dmiles8@mail.ccsf.edu>
2024-01-04 19:22:44 -0300

    Add files

---
+++ Shared_vars.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import os
+import json
+mem = {}
+vismem = {}
+blipcache = {}
+class Config:
+    def __init__(self):
+        script_dir = Path(os.path.abspath(__file__)).parent
+
+        with open(os.path.join(script_dir, 'config.json')) as config_file:
+            config = json.load(config_file)
+
+        self.backend = config['Backend']
+        self.host = config['HOST']
+        self.port = config['PORT']
+        self.base_context = config['Base_Context']
+        self.model_name = config['model_Name']
+        self.enabled_features = config['Enabled_features']
+        self.llm_parameters = config['LLM_parameters']
+        self.adminip = config['admin_ip']
+        self.api_key = config['api_key']
+        self.ctxlen = config['max_seq_len']
+        self.reservespace = config['reserve_space']
+        print("Loaded config")
+
+config = Config()
+API_ENDPOINT_URI = f"http://{config.host}:{config.port}/"
+API_KEY = config.api_key 
+
+TABBY = False
+if config.backend == "tabbyapi":
+    TABBY = True
